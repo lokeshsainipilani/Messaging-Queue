c0b7240a0f218931f463438ff0bc7d75122a890f
Lokesh Saini <lokeshsainipilani@gmail.com>
2024-12-25 21:48:18 +0530

    add initial files

---
+++ express_backend/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express"
+import { createClient } from "redis"
+
+const app = express();
+app.use(express.json());
+
+const client = createClient();
+client.connect()
+
+app.post("/submit", async (req,res)=>{
+    const {problemId, userId, code, language}= req.body;
+    await client.lPush("submissions", JSON.stringify({problemId, userId, code, language}))
+    res.json({
+        message:"submission received"
+    })
+})
+
+app.listen(3000)
